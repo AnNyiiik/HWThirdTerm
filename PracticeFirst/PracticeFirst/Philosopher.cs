5ba94ce4c74445781a6a7022e405e81bcef811d1
AnNyiiik <anna.nikolaevakons@gmail.com>
2023-09-25 21:30:09 +0300

    practice

---
+++ PracticeFirst/PracticeFirst/Philosopher.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+namespace PracticeFirst;
+
+public class Philosopher
+{
+	private bool isThinking;
+	private bool isEating;
+	private bool doneEating;
+	private bool doneThinking;
+	private int number;
+
+	public bool IsThinking { get => isThinking; }
+	public bool IsEating { get => isEating; }
+	public bool DoneEating { get => doneEating; }
+	public bool DoneThinking { get => doneThinking; }
+
+    public void Eat()
+	{
+		doneThinking = false;
+		doneEating = false;
+        Volatile.Write(ref isEating, true);
+        Console.WriteLine("Currently the philosopher {0} is eating", number);
+		Thread.Sleep(1000);
+		isEating = false;
+		Volatile.Write(ref doneEating, true);
+    }
+
+	public void Right()
+	{
+		Thread.Sleep(100);
+	}
+
+	public void Left()
+	{
+        Thread.Sleep(100);
+    }
+
+    public void Think()
+    {
+		doneEating = false;
+		doneThinking = false;
+		Volatile.Write(ref isThinking, true);
+        Console.WriteLine("Currently the philosopher {0} is thinking", number);
+        Thread.Sleep(1000);
+		isThinking = false;
+        Volatile.Write(ref doneThinking, true);
+    }
+
+    public Philosopher(int number)
+	{
+		this.number = number;
+	}
+}
+
---
+++ PracticeFirst/PracticeFirst/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Threading;
+using PracticeFirst;
+// See https://aka.ms/new-console-template for more information
+var threads = new Thread[5];
+var availableForks = new int[10];
+var synchronizationObject = new Object();
+var philosophers = new Philosopher[5];
+
+for (var i = 0; i < 10; ++i)
+{
+    availableForks[i] = 1;
+}
+
+for (var i = 0; i < 5; ++i)
+{
+    philosophers[i] = new Philosopher(i);
+}
+
+for (var i = 0; i < 5; ++i)
+{
+    var localI = i;
+    threads[localI] = new Thread(() =>
+    {
+        lock (synchronizationObject)
+        {
+            if (Volatile.Read(ref availableForks[localI * 2]) == 1 &&
+            availableForks[localI * 2 + 1] == 1 && philosophers[localI].DoneThinking)
+            {
+                philosophers[localI].Left();
+                Volatile.Write(ref availableForks[localI * 2], 0);
+                philosophers[localI].Right();
+                Volatile.Write(ref availableForks[localI * 2 + 1], 0);
+                philosophers[localI].Eat();
+            }
+            else if (philosophers[localI].DoneEating)
+            {
+                philosophers[localI].Left();
+                Volatile.Write(ref availableForks[localI * 2], 1);
+                philosophers[localI].Right();
+                Volatile.Write(ref availableForks[localI * 2 + 1], 1);
+                philosophers[localI].Think();
+            };
+        }
+    });
+}
+
+Console.WriteLine("Hello world");
+
+for (var i = 0; i < 5; ++i)
+{
+    threads[i].Start();
+}
+
+for (var i = 0; i < 5; ++i)
+{
+    threads[i].Join();
+}
