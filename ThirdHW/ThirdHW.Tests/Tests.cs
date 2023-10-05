d5de66b7eab82d502f414f17e888f9abe59697ee
AnNyiiik <anna.nikolaevakons@gmail.com>
2023-10-06 00:58:39 +0300

    changed

---
+++ ThirdHW/ThirdHW.Tests/Tests.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+using ThirdHW;
+namespace ThirdHW.Tests;
+
+public class Tests
+{
+    private MyThreadPool myThreadPool;
+    private ManualResetEvent manualResetEvent;
+    private const int threadSize = 8;
+    [SetUp]
+    public void Setup()
+    {
+        myThreadPool = new MyThreadPool(threadSize);
+        manualResetEvent = new ManualResetEvent(false);
+    }
+
+    [Test]
+    public void AreAllTheThreadsWorkingTest()
+    {
+        for (var i = 0; i < threadSize; ++i)
+        {
+            myThreadPool.AddTask(() =>
+            {
+                manualResetEvent.WaitOne();
+            })
+        }
+        Thread.Sleep(100);
+        Assert.AreEqual(threadSize, myThreadPool.WorkingThreads);
+    }
+}
