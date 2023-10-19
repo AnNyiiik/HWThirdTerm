ecef50b9a2dbcda5803e95bf78de369d29c4d9cd
AnNyiiik <anna.nikolaevakons@gmail.com>
2023-10-19 13:46:37 +0300

    added client and server code

---
+++ FourthHW/FourthHW.Tests/Test.cs
@@ -0,0 +1,35 @@
+﻿namespace FourthHW.Tests;
+
+public class Tests
+{
+    private Server server;
+    private Client client;
+    [SetUp]
+    public void Setup()
+    {
+        server = new Server(21, Directory.GetCurrentDirectory());
+        client = new Client();
+    }
+
+    [Test]
+    public void ClientTestList()
+    {
+        var result = server.List("../../../../TestDirectory");
+        Assert.That(result.Item1 == 3);
+        var correctAnswer = new List<(string, bool)>();
+        correctAnswer.Add(("TestDirectory", true));
+        correctAnswer.Add(("TestFile1.txt", false));
+        correctAnswer.Add(("TestFile2.txt", false));
+        Assert.NotNull(result.Item2);
+        if (result.Item2 != null)
+        {
+            Assert.Equals(result.Item2, correctAnswer);
+        }
+    }
+
+    [Test]
+    public void ServerTest()
+    {
+
+    }
+}
---
+++ FourthHW/FourthHW/Client.cs
@@ -0,0 +1,59 @@
+﻿namespace FourthHW;
+
+public class Client
+{
+    private Server? currentServer;
+    private int port;
+
+    public bool GetServer(Server server, int port)
+    {
+        var returnedPort = server.GetPort(port);
+        if (returnedPort != -1)
+        {
+            currentServer = server;
+            this.port = port;
+        }
+        return returnedPort != -1;
+    }
+
+    public void LeaveServer()
+    {
+        currentServer?.ReturnPort(port);
+        currentServer = null;
+    }
+
+    public (int size, List<(string, bool)>?) List(Server server, int port, string path)
+    {
+        if (server != currentServer && port != this.port)
+        {
+            LeaveServer();
+            GetServer(server, port);
+        }
+
+        if (currentServer != null && port != -1)
+        {
+            return currentServer.List(path);
+        } else
+        {
+            throw new ArgumentException();
+        }
+    }
+
+    public (long size, byte[]? bytes) Get(Server server, int port, string path)
+    {
+        if (server != currentServer && port != this.port)
+        {
+            LeaveServer();
+            GetServer(server, port);
+        }
+
+        if (currentServer != null && port != -1)
+        {
+            return currentServer.Get(path);
+        }
+        else
+        {
+            throw new ArgumentException();
+        }
+    }
+}
