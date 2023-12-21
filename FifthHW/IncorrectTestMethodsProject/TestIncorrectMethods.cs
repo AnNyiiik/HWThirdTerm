8f02437ddfe61d58000bae62bb555e249c3a67a0
AnNyiiik <anna.nikolaevakons@gmail.com>
2023-12-21 15:18:16 +0300

    changed

---
+++ FifthHW/IncorrectTestMethodsProject/TestIncorrectMethods.cs
@@ -0,0 +1,89 @@
+﻿namespace IncorrectTestMethodsProject;
+using FifthHW;
+
+public class TestIncorrectMethods
+{
+    [MyTest]
+    public void TestsMethodFailed()
+    {
+        throw new Exception();
+    }
+
+    [MyTest]
+    public void TestMethodPassed()
+    {
+
+    }
+
+    [MyTest]
+    public int TestNotVoidReturnValue()
+    {
+        return 0;
+    }
+
+    [MyTest]
+    public void TestWithInputParameters(int parameter)
+    {
+
+    }
+
+    [BeforeClass]
+    public void BeforeClassNotStatic()
+    {
+
+    }
+
+    [BeforeClass]
+    public void AfterClassNotStatic()
+    {
+
+    }
+
+    [BeforeClass]
+    public static void BeforeClassWithInputParameters(int parameter)
+    {
+
+    }
+
+    [AfterClass]
+    public static void AfterClassWithInputParameters(int parameter)
+    {
+
+    }
+
+    [BeforeClass]
+    public static int BeforeClassNotVoidReturnValue()
+    {
+        return 0;
+    }
+
+    [AfterClass]
+    public static int AfterClassNotVoidReturnValue()
+    {
+        return 0;
+    }
+
+    [Before]
+    public int BeforeNotVoidReturnValue()
+    {
+        return 0;
+    }
+
+    [After]
+    public int AfterNotVoidReturnValue()
+    {
+        return 0;
+    }
+
+    [Before]
+    public void BeforeWithInputParameters(int parameter)
+    {
+        
+    }
+
+    [After]
+    public void AfterWithInputParameters(int parameter)
+    {
+        
+    }
+}
---
+++ FifthHW/MyNUnitTestProject/TestClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace MyNUnitTestProject;
+
+public class TestClass
+{
+	public TestClass()
+	{
+	}
+
+
+}
+
